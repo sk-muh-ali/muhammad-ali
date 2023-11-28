95f2e9be076bb0cf4622bbdf0888a3db348084f8
sk-muh-ali <152100785+sk-muh-ali@users.noreply.github.com>
2023-11-28 20:05:20 +0500

    Update java.js

--- java.js
+++ java.js
@@ -1,19 +1,19 @@
-const body = document.querySelector("body");
-body.innerHTML += '<div id="crsr"></div>';
-let crsr = document.querySelector("#crsr");
-
-crsr.style.width = "30px";
-crsr.style.height = "30px";
-crsr.style.backgroundColor = "transparent";
-crsr.style.position = "fixed";
-crsr.style.pointerevents = "none";
-crsr.style.transform ="translate(-0%, -10%)";
-crsr.style.border = "2px solid #0ef";
-crsr.style.boxShadow = "0 0 7px #0ef"
-crsr.style.borderRadius = "50%";
-crsr.style.zIndex = "999";
-
-body.addEventListener("mousemove", function(e){
-    crsr.style.top = e.y -25 + "px";
-    crsr.style.left = e.x -25 + "px";
-});
+        const body = document.querySelector("body");
+        body.innerHTML += '<div id="crsr"></div>';
+        let crsr = document.querySelector("#crsr");
+        
+        crsr.style.width = "30px";
+        crsr.style.height = "30px";
+        crsr.style.backgroundColor = "transparent";
+        crsr.style.position = "fixed";
+        crsr.style.pointerevents = "none";
+        crsr.style.transform ="translate(-0%, -10%)";
+        crsr.style.border = "2px solid #0ef";
+        crsr.style.boxShadow = "0 0 7px #0ef"
+        crsr.style.borderRadius = "50%";
+        crsr.style.zIndex = "999";
+        
+        body.addEventListener("mousemove", function(e){
+            crsr.style.top = e.y -25 + "px";
+            crsr.style.left = e.x -25 + "px";
+        });
