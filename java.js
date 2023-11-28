7f392d94600e02b717be0c94a75ac545af85dc66
sk-muh-ali <152100785+sk-muh-ali@users.noreply.github.com>
2023-11-28 19:57:45 +0500

    commit

--- java.js
+++ java.js
@@ -7,10 +7,11 @@ crsr.style.height = "30px";
 crsr.style.backgroundColor = "transparent";
 crsr.style.position = "fixed";
 crsr.style.pointerevents = "none";
-crsr.style.transform ="translate(-30%, -10%)";
-crsr.style.border = "2px solid red";
+crsr.style.transform ="translate(-0%, -10%)";
+crsr.style.border = "2px solid #0ef";
+crsr.style.boxShadow = "0 0 7px #0ef"
 crsr.style.borderRadius = "50%";
-crsr.style.zIndex = "99";
+crsr.style.zIndex = "999";
 
 body.addEventListener("mousemove", function(e){
     crsr.style.top = e.y -25 + "px";
