f1d19d8bdb7e5e703d4ad07316b7555f7e34c425
sk-muh-ali <152100785+sk-muh-ali@users.noreply.github.com>
2023-12-01 19:23:13 +0500

    Update java.js

--- java.js
+++ java.js
@@ -7,13 +7,14 @@
         crsr.style.backgroundColor = "transparent";
         crsr.style.position = "fixed";
         crsr.style.pointerevents = "none";
-        crsr.style.transform ="translate(-0%, -10%)";
+        crsr.style.transform ="translate(0%, 0%)";
         crsr.style.border = "2px solid #0ef";
         crsr.style.boxShadow = "0 0 7px #0ef"
         crsr.style.borderRadius = "50%";
         crsr.style.zIndex = "999";
+        crsr.style.pointerEvents = "none";
         
         body.addEventListener("mousemove", function(e){
-            crsr.style.top = e.y -25 + "px";
-            crsr.style.left = e.x -25 + "px";
+            crsr.style.top = e.y -15 + "px";
+            crsr.style.left = e.x -15 + "px";
         });
